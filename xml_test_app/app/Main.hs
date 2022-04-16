a1fd16e4225945193f67f63172b00d76016d3a50
arachnothrone <arachnothrone@gmail.com>
2022-04-15 23:09:39 -0400

    Total genetic reconstruction

--- xml_test_app/app/Main.hs
+++ xml_test_app/app/Main.hs
@@ -3,141 +3,54 @@
 
 module Main where
 
-import qualified Data.Map        as M
-import           Prelude         hiding (readFile, writeFile)
-import           Text.Hamlet.XML
-import           Text.XML
-import           Data.Text
-import Data.Foldable
+import qualified    Data.Map        as M
+import              Prelude         hiding (readFile, writeFile)
+import              Text.Hamlet.XML
+import              Text.XML
+import              Data.Text
+import              Data.Foldable
 
 -- import qualified Data.Map.Strict as Map
 -- import Data.Map (Map())
 
-main :: IO ()
+main :: IO [(String, String)]
 main = do
     -- readFile will throw any parse errors as runtime exceptions
     Document prologue root epilogue <- readFile def "src_data/route_example_1.gpx"
-
-    -- -- root is the root element of the document, let's modify it
-    -- let root' = transform root
-    let rootListTups = getElevations root
-
+    let resultingValues = procMain root
     --putStrLn "theend"
-    return()
-
-    
-
-    -- -- And now we write out. Let's indent our output
-    -- writeFile def
-    --     { rsPretty = True
-    --     } "output.html" $ Document prologue root' epilogue
-
---getElevations :: Element -> [(String, String)]     -- Tuple (timeStamp, elevation)
-getElevations :: Element -> [[Node]]
-getElevations (Element name attribs children) = [goNode child | child <- children]
-
--- -- We'll turn out <document> into an XHTML document
--- transform :: Element -> Element
--- transform (Element _name attrs children) = Element "html" M.empty
---     [xml|
---         <head>
---             <title>
---                 $maybe title <- M.lookup "title" attrs
---                     \#{title}
---                 $nothing
---                     Untitled Document
---         <body>
---             $forall child <- children
---                 ^{goNode child}
---      |]
-
--- elmTpl :: (Element, [String]) -> Element
--- elmTpl (a, _) = a 
-
-getElevations2 :: Element -> [(String, String)]
-getElevations2 (Element name attribs children) = Data.Foldable.concat ge2
-    where
-        (ge1, ge2) = unzip [goNode' c [] | c <- children]
-
-goNode :: Node -> [Node]
-goNode (NodeElement e) = [NodeElement $ goElem e]
-goNode (NodeContent t) = [NodeContent t]
-goNode (NodeComment _) = [] -- hide comments
-goNode (NodeInstruction _) = [] -- and hide processing instructions too
-
-goNode' :: Node -> [(String, String)] -> ([Node], [(String, String)])
--- goNode' :: Node -> [Node]
-goNode' (NodeElement e) xs = ([NodeElement ge1], ge2)
-    where 
-        (ge1, ge2) = goElem' e xs
-goNode' (NodeContent t) xs = ([NodeContent t], xs)
-goNode' (NodeComment _) _ = ([], []) -- hide comments
-goNode' (NodeInstruction _) _ = ([], []) -- and hide processing instructions too
-
-gnlist :: [Node] -> [(String, String)] -> [Node]
-gnlist nlst _ = Data.Foldable.concat g1
-    where
-        (g1, _) = unzip [goNode' c [] | c <- nlst]
-    
-        
-
-goElem' :: Element -> [(String, String)] -> (Element, [(String, String)])
-goElem' (Element (Name elmName n2 n3) attrs children) xs 
-    | elmName == pack "speed" = (
-        -- Element "--------------> speed" attrs $ Data.Foldable.concatMap (goNode' []) children, ("speed", getNodeContent children):xs
-        Element "--------------> speed" attrs $ gnlist children [], ("speed", getNodeContent children):xs)
-            -- where 
-            --     (gn1, gn2) = unzip [goNode' c [] | c <- children] 
-        
-    --   | otherwise = (Element (Name elmName n2 n3) attrs children, xs)
-    | otherwise = (Element "" (M.fromList []) [], xs)
+    return(resultingValues)        
 
 getNodeContent :: [Node] -> String
 getNodeContent (x:xs) = unpack $ getN x 
     where 
         getN (NodeContent c) = c 
 
--- convert each source element to its XHTML equivalent
-goElem :: Element -> Element
--- goElem :: Element -> (Element, [String])
-goElem (Element (Name elmName n2 n3) attrs children)
-    -- | elmName == pack "trk" = Element "--------------> trk" attrs $ Data.Foldable.concatMap goNode children -- trkseg
-    -- | elmName == pack "trkseg" = Element "--------------> trkseg" attrs $ Data.Foldable.concatMap goNode children -- trkpt
-    -- | elmName == pack "trkpt" = Element "--------------> trkpt" attrs $ Data.Foldable.concatMap goNode children -- trkpt
-    -- | elmName == pack "time" = Element "--------------> time" attrs $ Data.Foldable.concatMap goNode children -- extensions
-    -- | elmName == pack "extensions" = Element "--------------> extensions" attrs $ Data.Foldable.concatMap goNode children -- extensions
-    | elmName == pack "speed" = Element "--------------> speed" attrs $ Data.Foldable.concatMap goNode children
-    | elmName == pack "hAcc" = Element "--------------> hAcc" attrs $ Data.Foldable.concatMap goNode children
-    | elmName == pack "vAcc" = Element "--------------> vAcc" attrs $ Data.Foldable.concatMap goNode children
-    -- | otherwise = let n = (unpack elmName ++ ": -----------> 000") in 
-    --     Element 
-    --         Name {
-    --             nameLocalName = pack n, 
-    --             nameNamespace = Nothing, 
-    --             namePrefix = Nothing
-    --         }
-    --         attrs $ Data.Foldable.concatMap goNode children
-    | otherwise = --Element "" (M.fromList []) []
-        Element (Name elmName n2 n3) attrs $ Data.Foldable.concatMap goNode children
+procElem :: Element -> [(String, String)] -> (Element, [(String, String)])
+procElem (Element (Name elmName n2 n3) attrs children) xs 
+    | elmName == pack "speed" = (emptyElement, ("Speed", value):xs)
+    | elmName == pack "hAcc" = (emptyElement, ("hAcc", value):xs)
+    | elmName == pack "vAcc" = (emptyElement, ("vAcc", value):xs)
+    | elmName == pack "name" = (emptyElement, ("name", value):xs)
+    | elmName == pack "ele" = (emptyElement, ("ele", value):xs)
+    | otherwise = (Element (Name elmName n2 n3) attrs $ Data.Foldable.concat processedChildren, Data.Foldable.concat xs')
+    where
+        value = getNodeContent children
+        emptyElement = Element "" (M.fromList []) []
+        (processedChildren, xs') = unzip [procNode c xs | c <- children]
 
--- -- goElem (Element "para" attrs children) =
--- --     Element "p" attrs $ concatMap goNode children
--- -- goElem (Element "em" attrs children) =
--- --     Element "i" attrs $ concatMap goNode children
--- -- goElem (Element "strong" attrs children) =
--- --     Element "b" attrs $ concatMap goNode children
--- -- goElem (Element "image" attrs _children) =
--- --     Element "img" (fixAttr attrs) [] -- images can't have children
--- --   where
--- --     fixAttr mattrs
--- --         | "href" `M.member` mattrs  = M.delete "href" $ M.insert "src" (mattrs M.! "href") mattrs
--- --         | otherwise                 = mattrs
--- goElem (Element name attrs children) =
---     -- don't know what to do, just pass it through...
---     Element name attrs $ concatMap goNode children
+procNode :: Node -> [(String, String)] -> ([Node], [(String, String)])
+procNode (NodeElement e) xs = ([NodeElement ge1], ge2)
+    where
+        (ge1, ge2) = procElem e xs
+procNode (NodeContent t) xs = ([NodeContent t], xs)
+procNode (NodeComment _) _ = ([], []) -- hide comments
+procNode (NodeInstruction _) _ = ([], []) -- and hide processing instructions too
 
--- -- goElem (Element name attrs children) = 
--- --     Element "sdf" attrs []
+procMain :: Element -> [(String, String)]
+procMain rootElement = dataResult
+    where
+        (_, dataResult) = procElem rootElement []
 
 -- map with ints as keys and strings as values
 myMap :: M.Map Int String
