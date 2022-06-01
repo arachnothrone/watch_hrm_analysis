63748cb35b34453120dafd6128403b5767a81dce
arachnothrone <arachnothrone@gmail.com>
2022-06-01 16:37:44 -0400

    Add data type for data point

--- xml_test_app/app/Main.hs
+++ xml_test_app/app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+-- {-# LANGUAGE GADTs             #-}
 
 module Main where
 
@@ -15,32 +16,37 @@ import              Data.List
 -- import qualified Data.Map.Strict as Map
 -- import Data.Map (Map())
 
-main :: IO [(String, String)]
+data ValuePoint = ValuePoint 
+    { paramName     :: String
+    , paramValue    :: String
+    } deriving (Show)
+
+--main :: IO [(String, String)]
+main :: IO [ValuePoint]
 main = do
     -- readFile will throw any parse errors as runtime exceptions
     Document prologue root epilogue <- readFile def "src_data/route_example_1.gpx"
     Document proFull rootFull epiFull <- readFile def "src_data/apple_health_export/workout-routes/route_2020-07-11_8.04pm.gpx"
     let resultingValues = procMain root
-
     putStrLn $ Data.List.unlines $ Data.List.map showDataLine resultingValues
-    return(resultingValues)        
+    return(resultingValues)
 
-showDataLine :: (String, String) -> String
-showDataLine (name, value) = printf "%7s = %s" name value
+showDataLine :: ValuePoint -> String
+showDataLine (ValuePoint name value) = printf "%7s = %s" name value
 
 getNodeContent :: [Node] -> String
 getNodeContent (x:xs) = unpack $ getN x 
     where 
         getN (NodeContent c) = c 
 
-procElem :: Element -> [(String, String)] -> (Element, [(String, String)])
+procElem :: Element -> [ValuePoint] -> (Element, [ValuePoint])
 procElem (Element (Name elmName n2 n3) attrs children) xs 
     | elmName == pack "trkpt" = procElem 
                                     (Element "Trackpoint" (M.fromList []) $ Data.Foldable.concat processedChildren) $ 
                                     Data.Foldable.concat xs'
-    | elmName == pack "speed" = (emptyElement, ("Speed", value):xs)
-    | elmName == pack "name" = (emptyElement, ("name", value):xs)
-    | elmName == pack "time" = (emptyElement, ("time", value):xs)
+    | elmName == pack "speed" = (emptyElement, (ValuePoint "Speed" value):xs)
+    | elmName == pack "name" = (emptyElement, (ValuePoint "name" value):xs)
+    | elmName == pack "time" = (emptyElement, (ValuePoint "time" value):xs)
     -- | elmName == pack "hAcc" = (emptyElement, ("hAcc", value):xs)
     -- | elmName == pack "vAcc" = (emptyElement, ("vAcc", value):xs)
     -- | elmName == pack "ele" = (emptyElement, ("ele", value):xs)
@@ -50,7 +56,7 @@ procElem (Element (Name elmName n2 n3) attrs children) xs
         emptyElement = Element "" (M.fromList []) []
         (processedChildren, xs') = unzip [procNode c xs | c <- children]
 
-procNode :: Node -> [(String, String)] -> ([Node], [(String, String)])
+procNode :: Node -> [ValuePoint] -> ([Node], [ValuePoint])
 procNode (NodeElement e) xs = ([NodeElement ge1], ge2)
     where
         (ge1, ge2) = procElem e xs
@@ -58,7 +64,7 @@ procNode (NodeContent t) xs = ([NodeContent t], xs)
 procNode (NodeComment _) _ = ([], [])           -- hide comments
 procNode (NodeInstruction _) _ = ([], [])       -- hide processing instructions
 
-procMain :: Element -> [(String, String)]
+procMain :: Element -> [ValuePoint]
 procMain rootElement = dataResult
     where
         (_, dataResult) = procElem rootElement []
